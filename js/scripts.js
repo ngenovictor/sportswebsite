72d47b679a6d81e7858c629c0bc6e4c6f0ed782c
elvis <mosoti17@gmail.com>
2017-06-19 16:42:09 +0300

    opacity when navbar is opened

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 
 });
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "300px";
+    $("div#main").css("opacity","0.5");
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    $("div#main").css("opacity","1");
 }
