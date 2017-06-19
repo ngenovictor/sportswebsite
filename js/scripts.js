0edf69ec410974dd8dc905e644627e0eae4ac2dd
elvis <mosoti17@gmail.com>
2017-06-19 14:27:27 +0300

    fullpage nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  
+
 });
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
