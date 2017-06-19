a59af2d7075c82f0213013a9c21bf14a9ad5d750
Victor Ngeno <ngenovictor321@gmail.com>
2017-06-19 16:54:39 +0300

    create working prototype for map info

--- js/map.js
+++ js/map.js
@@ -1,11 +1,39 @@
-
-map = L.map('map').setView([-1.2921,36.8219], 12);
-
+var map = L.map('map');
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoibmdlbm92aWN0b3IzMjEiLCJhIjoiY2lmeWhzejVlMDJoaXU0bHp2NDcza2NteiJ9.xbQkW7UhxjnDXZT9ZQS1Rg',
 {
     id: 'mapbox.light',
 }).addTo(map);
-//
-var data = $.getJSON("facilities.json")
-L.geoJson(data).addTo(map);
+
+
+$(document).ready(function(){
+  function addDataToMap(data, map) {
+      var dataLayer = L.geoJson(data, {
+          onEachFeature: function(feature, layer) {
+              var popupText = "<div id='popup' >Name: " + feature.properties.Name
+                  + "<br>Type: " + feature.properties.Type+"</div>";
+              layer.bindPopup(popupText);
+
+            }
+          });
+      dataLayer.addTo(map);
+      console.log()
+  }
+  addDataToMap(facilities, map);
+
+  var facilityLayer = L.geoJson(facilities).addTo(map);
+  var routesLayer = L.geoJson(routes).addTo(map);
+  map.fitBounds(facilityLayer.getBounds(),routesLayer.getBounds());
+
+  $("section#mapinfo .row .cols .mapinfo#routes").click(function(){
+    $("section#mapinfo .row .cols #mapresult").show();
+    $("section#mapinfo .row .cols #mapresult").text("");
+    L.geoJson(routes,{
+      onEachFeature: function(feature, layer){
+        $("section#mapinfo .row .cols #mapresult").append("<p>Route:"+feature.properties.Name+"</p>").click(function(){
+          map.fitBounds(layer.getBounds());
+        })
+      }
+    })
+  })
+})
